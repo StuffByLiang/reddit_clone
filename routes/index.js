6537309495cde132c6c303bb9a9c6876285e4339
Liang Liu <stuffbyliang@gmail.com>
2018-12-28 18:47:46 -0800

    added npm scripts such as npm build and npm start & optmized delivery using a CDN

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
+const CDN = (process.env.TURBO_ENV == 'dev') ? '' : process.env.TURBO_CDN;
+
 /*  This is the home route. It renders the index.mustache page from the views directory.
 	Data is rendered using the Mustache templating engine. For more
 	information, view here: https://mustache.github.io/#demo */
 router.get('/', (req, res) => {
-	res.render('index', null)
+
+	res.render('index', {
+		cdn: CDN
+	})
+	
 })
 
 router.get('/rooms', (req, res) => {
