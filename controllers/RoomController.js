222860bbe7dfab3ae6e4c1ab190ca36197abbdab
Liang Liu <stuffbyliang@gmail.com>
2018-12-31 01:44:14 -0800

    added slugs to room schema in database

--- controllers/RoomController.js
+++ controllers/RoomController.js
@@ -2,6 +2,25 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const Promise = require('bluebird');
 const resource = 'room';
 
+const slugify = function(text){
+ 	return text.toString().toLowerCase()
+			.replace(/\s+/g, '-')           // Replace spaces with -
+			.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+			.replace(/\-\-+/g, '-')         // Replace multiple - with single -
+			.replace(/^-+/, '')             // Trim - from start of text
+			.replace(/-+$/, '');            // Trim - from end of text
+}
+
+const randomString = function(numChars) {
+    var randomString = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i=0; i < numChars; i++) {
+      randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return randomString;
+}
+
 module.exports = {
   get: (params) => {
     return new Promise((resolve, reject) => {
@@ -29,7 +48,8 @@ module.exports = {
 
   post: (body) => {
     return new Promise((resolve, reject) => {
-      body['subscribers'] = [body.user] //add the user id to room subscribers
+      body['subscribers'] = [body.user]; //add the user id to room subscribers
+      body['slug'] = slugify(body.category.toLowerCase())+'-'+randomString(6);
 
       //then fetch current user
       turbo.fetchOne('user', body.user)
